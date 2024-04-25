65167f5fee1d0e0d823975c16887705236d9e53e
NTFail <55147550+NTFail@users.noreply.github.com>
2024-04-25 15:09:08 +0200

    Removing counter, cleanup, adjusting settings

--- EssenceMemoryKillCounter.cs
+++ EssenceMemoryKillCounter.cs
@@ -13,7 +13,6 @@ namespace EssenceMemoryKillCounter
     public class EssenceMemoryKillCounter : BaseSettingsPlugin<Settings>
     {
 
-        Dictionary<string, List<Essence>> essenceCounter = new Dictionary<string, List<Essence>>();
         Dictionary<string, List<Essence>> essenceKilled = new Dictionary<string, List<Essence>>();
         private bool _canTick = true;
         private Camera Camera => GameController.IngameState.Camera;
@@ -31,36 +30,10 @@ namespace EssenceMemoryKillCounter
         {
             if (_ingameUiCheckVisible?.Value == true || Camera == null || GameController.Area.CurrentArea.IsTown || GameController.Area.CurrentArea.IsHideout)
             {
-                _canTick = false;
                 return;
             }
-            if (essenceCounter.Count > 0)
-            {
-                var toRemove = essenceCounter.Where(pair => pair.Key != area.Name)
-                                         .Select(pair => pair.Key)
-                                         .ToList();
 
-                foreach (var key in toRemove)
-                {
-                    essenceKilled.Remove(key);
-                }
-            }
-
-            if (essenceKilled.Count > 0)
-            {
-                var toRemove = essenceKilled.Where(pair => pair.Key != area.Name)
-                                         .Select(pair => pair.Key)
-                                         .ToList();
-
-                foreach (var key in toRemove)
-                {
-                    essenceKilled.Remove(key);
-                }
-            }
-            if (!essenceCounter.TryGetValue(area.Name, out var valueCounter))
-            {
-                essenceCounter.Add(area.Name, []);
-            }
+            cleanZoneDict(area.Name, essenceKilled);
             if (!essenceKilled.TryGetValue(area.Name, out var valueKilled))
             {
                 essenceKilled.Add(area.Name, []);
@@ -69,11 +42,9 @@ namespace EssenceMemoryKillCounter
 
         public override Job Tick()
         {
-            _canTick = true;
             if (_ingameUiCheckVisible?.Value == true ||
                 Camera == null || GameController.Area.CurrentArea.IsTown || GameController.Area.CurrentArea.IsHideout)
             {
-                _canTick = false;
                 return null;
             }
 
@@ -86,16 +57,9 @@ namespace EssenceMemoryKillCounter
                 if (essence == null) continue;
                 try
                 {
-                    if (essenceKilled[areaName].Where(ess=> ess.Id.Equals(essence.Id)).Count() == 0 && essence.IsCannotBeDamaged && !essence.IsDead)
-                    {
-                        
-                        essenceCounter[areaName].Add(essence);
-                    }
-                    
-                    if ((essenceKilled[areaName].Where(ess => ess.Id.Equals(essence.Id)).Count() == 0 && essence.IsDead))
+                    if (essenceKilled[areaName].Where(ess => ess.Id.Equals(essence.Id)).Count() == 0 && essence.IsDead)
                     {
                         essenceKilled[areaName].Add(essence);
-                        essenceCounter[areaName].Remove(essence);
                     }
                 }
                 catch (Exception e)
@@ -111,7 +75,7 @@ namespace EssenceMemoryKillCounter
             if (!_canTick) return;
             var area = GameController.Area;
             var essenceText = $"Essence Killed: {essenceKilled[area.CurrentArea.Name].Count()}";
-            if (Settings.ShowKilledEssenceExileName && essenceKilled.Count(se => se.Key.Equals(area.CurrentArea.Name)) > 0)
+            if (Settings.ShowKilledEssenceExileName && essenceKilled.Count(essk => essk.Key.Equals(area.CurrentArea.Name) && essk.Value.Count > 1) > 0)
             {
                 essenceText += $"\nExile killed:";
                     foreach (var exile in essenceKilled[area.CurrentArea.Name])
@@ -124,7 +88,7 @@ namespace EssenceMemoryKillCounter
                 var bgSize = Settings.BackgroundSize.Value;
                 var box = Graphics.DrawText(essenceText, pos, Settings.TextColor);
                 Graphics.DrawBox(pos - new Vector2(bgSize, bgSize), pos + new Vector2(box.X, box.Y) + new Vector2(bgSize, bgSize), Settings.BackgroundColor, 2);
-            
+
         }
 
         public override void EntityAdded(Entity entity)
@@ -137,11 +101,26 @@ namespace EssenceMemoryKillCounter
             }
         }
 
-        public bool CannotBeDamaged(Entity entity)
+        private bool CannotBeDamaged(Entity entity)
         {
             var entityStat = entity.TryGetComponent<Stats>(out var exileStat) ? exileStat?.StatDictionary : null;
             var isExileCannotBeDamaged = entityStat.TryGetValue(GameStat.CannotBeDamaged, out var cannotBeDamaged);
             return isExileCannotBeDamaged;
+        }
+
+        private void cleanZoneDict(string areaName, Dictionary<string, List<Essence>> dictZone)
+        {
+            if (dictZone.Count > 0)
+            {
+                var toRemove = dictZone.Where(pair => pair.Key != areaName)
+                                         .Select(pair => pair.Key)
+                                         .ToList();
+
+                foreach (var key in toRemove)
+                {
+                    dictZone.Remove(key);
+                }
+            }
         }
     }
 }
--- Settings.cs
+++ Settings.cs
@@ -1,6 +1,7 @@
 ﻿using ExileCore.Shared.Attributes;
 using ExileCore.Shared.Interfaces;
 using ExileCore.Shared.Nodes;
+using System.Drawing;
 
 namespace EssenceMemoryKillCounter
 {
@@ -10,10 +11,10 @@ namespace EssenceMemoryKillCounter
         public RangeNode<int> PositionX { get; set; } = new(100, 0, 5000);
         public RangeNode<int> PositionY { get; set; } = new(100, 0, 5000);
         public RangeNode<int> BackgroundSize { get; set; } = new(6, 0, 15);
-        public ColorNode TextColor { get; set; } = new ColorNode(SharpDX.Color.Black);
-        public ColorNode BackgroundColor { get; set; } = new ColorNode(SharpDX.Color.White);
+        public ColorNode TextColor { get; set; } = new ColorNode(SharpDX.Color.White);
+        public ColorNode BackgroundColor { get; set; } = new ColorNode(new SharpDX.Color(255,255,255,89));
 
-        public ToggleNode ShowKilledEssenceExileName { get; set; } = new ToggleNode(false);
+        public ToggleNode ShowKilledEssenceExileName { get; set; } = new ToggleNode(true);
 
     }
 }
